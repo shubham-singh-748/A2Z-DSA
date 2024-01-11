abefcae127fa4d8060dbd60cbdeb1fb963d955f6
shubham-singh-748 <shubhamsingh74892@gmail.com>
2024-01-11 14:44:48 +0530

    added question 5

---
+++ patterns/Question 5.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    int a[n], b[n];
+
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = n; j > n - (n - i + 1); --j)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
